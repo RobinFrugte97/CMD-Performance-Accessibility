ecdd46b19cc8ef386609b85440bbbcd7cdc196d6
RobinFrugte97 <robin.frugte@hotmail.com>
2020-05-14 15:14:10 +0200

    seperated css files, added contact page

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const fetch = require('node-fetch')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,12 +8,10 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.get("/", (req, res) => {
-    fetch("https://www.cmd-amsterdam.nl/wp-json/wp/v2/pages/8901")
-        .then((resp) => resp.json())
-        .then(index => {
-            let html = index.content.rendered
-            res.render('index.ejs')
-        }).catch(err => console.log(err))
+    res.render('index.ejs')
+})
+app.get("/contact", (req, res) => {
+    res.render('contact.ejs')
 })
 
 app.listen(port, () => {
--- scripts/buildcss.js
+++ scripts/buildcss.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const minify = require('gulp-clean-css')
+const order = require("gulp-order")
 
 return gulp.src('./static/css/*.css')
+    .pipe(order([
+        "static/css/reset.css",
+        "static/css/header.css",
+        "static/css/*.css",
+        "static/css/footer.css"
+    ], { base: './' }))
     .pipe(concat('index.css'))
-    .pipe(minify())
+    // .pipe(minify())
     .pipe(gulp.dest('static/dist'))
